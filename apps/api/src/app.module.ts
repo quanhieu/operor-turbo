38c05324684a37b9a0ffd88403e1c4979ed95f84
Hieu Truong <hieutq1188@gmail.com>
2024-06-10 15:53:21 +0700

    feature/Init-source-base

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {
+  constructor() {}
+}
